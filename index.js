d1610877dbaecfed3cbae9f7614e9fd907b8a232
fotuka <antda08@yandex.ru>
2024-02-09 15:17:58 +0300

    Add class and index js file logic to color messages

---
+++ index.js
@@ -0,0 +1,20 @@
+import chalk from "chalk";
+import { Task } from "./models.js";
+
+const tasks = [
+    new Task(1, 'Сделать первую лабу', true),
+    new Task(2, 'Сделать вторую лабу', true),
+    new Task(3, 'Сделать третью лабу', false)
+] 
+
+PrintTasks();
+
+function PrintTasks()
+{
+    tasks.forEach((element) => {
+        if(element.status === false)
+            console.log(chalk.red(element.description))
+        else
+            console.log(chalk.green(element.description))
+    });
+}
